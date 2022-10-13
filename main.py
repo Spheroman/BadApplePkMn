2c90648b7221c51eb910e88d490e2024c482d972
Spheroman <jackwen04@gmail.com>
2022-10-13 23:57:55 +0800

    add frames
    made frames

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import numpy as np
 from pokemontcgsdk import Card
 from pokemontcgsdk import Set
 from pokemontcgsdk import Type
+from PIL import Image
+import os
+
+for filename in os.listdir('frames'):
+    image = Image.open(os.path.join('frames', filename))
+    new_image = image.resize((24, 12))
+    new_image.save(os.path.join('resizedFrames', filename))
